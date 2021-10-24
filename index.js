d9d5464296bccf8c6cd35398741f3df516236445
Kininaru <shiftregister233@outlook.com>
2021-10-24 19:33:57 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,15 @@
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    handleRequest(event.request).catch(
+      (err) => new Response(err.stack, { status: 500 })
+    )
+  );
+});
+
+async function handleRequest(request) {
+ let authCode = request.headers.get("Authorization");
+ if (authCode !== ShortLinkApi.get("code")) return new Response("Unauthorized operation.", {
+   status: 403
+ });
+ return new Response("This is 7nm.co api ctrl server.");
+}
