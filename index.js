3b851271c8b177fef336174d2dce1bcea8753453
Kininaru <shiftregister233@outlook.com>
2021-10-27 21:31:40 +0800

    format code

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 addEventListener("fetch", (event) => {
-  event.respondWith(
-    handleRequest(event.request).catch(
-      (err) => new Response(err.stack, {status: 500})
-    )
-  );
+    event.respondWith(
+        handleRequest(event.request).catch(
+            (err) => new Response(err.stack, {status: 500})
+        )
+    );
 });
 
 async function addLink(link) {
-  return "/link";
+    return "/link";
 }
 
 async function setLink(shortLink, link) {
-  if (await ShortLink.get(shortLink) !== null) return false;
-  ShortLink.put(shortLink, link);
-  return true;
+    if (await ShortLink.get(shortLink) !== null) return false;
+    ShortLink.put(shortLink, link);
+    return true;
 }
 
 function deleteLink(shortLink) {
-  ShortLink.delete(shortLink);
+    ShortLink.delete(shortLink);
 }
 
 async function handleRequest(request) {
-  if (request.headers.get("Authorization") !== ShortLinkApi.get("code")) return new Response("Unauthorized operation.", {status: 403});
-  let response = {code: 0};
-  let body = await request.json();
-  switch (request.url.split("/")[3]) {
-    case "":
-      response.msg = "This is 7nm.co api ctrl server.";
-      break;
-    case "add":
-      let link = await addLink(body.link);
-      if (link === null) response.code = 1;
-      else response.msg = link;
-      break;
-    case "set":
-      if (!setLink(body.shortLink, body.link)) response.code = 1;
-      break;
-    case "delete":
-      deleteLink(body.shortLink);
-      break;
-    default:
-      return new Response("Command not found.", {status: 404});
-  }
-  return new Response(response.msg);
+    if (request.headers.get("Authorization") !== ShortLinkApi.get("code")) return new Response("Unauthorized operation.", {status: 403});
+    let response = {code: 0};
+    let body = await request.json();
+    switch (request.url.split("/")[3]) {
+        case "":
+            response.msg = "This is 7nm.co api ctrl server.";
+            break;
+        case "add":
+            let link = await addLink(body.link);
+            if (link === null) response.code = 1;
+            else response.msg = link;
+            break;
+        case "set":
+            if (!setLink(body.shortLink, body.link)) response.code = 1;
+            break;
+        case "delete":
+            deleteLink(body.shortLink);
+            break;
+        default:
+            return new Response("Command not found.", {status: 404});
+    }
+    return new Response(response.msg);
 }
