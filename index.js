3652bc08ab22e509cbc555be6fe5abe27f9e668d
Kininaru <shiftregister233@outlook.com>
2021-10-27 21:01:08 +0800

    format

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 addEventListener("fetch", (event) => {
   event.respondWith(
     handleRequest(event.request).catch(
-      (err) => new Response(err.stack, { status: 500 })
+      (err) => new Response(err.stack, {status: 500})
     )
   );
 });
 
 async function handleRequest(request) {
- let authCode = request.headers.get("Authorization");
- if (authCode === undefined || authCode === null || authCode === "" || authCode !== ShortLinkApi.get("code")) return new Response("Unauthorized operation.", {
-   status: 403
- });
- return new Response("This is 7nm.co api ctrl server.");
+  if (request.headers.get("Authorization") !== ShortLinkApi.get("code")) return new Response("Unauthorized operation.", {
+    status: 403
+  });
+  return new Response("This is 7nm.co api ctrl server.");
 }
